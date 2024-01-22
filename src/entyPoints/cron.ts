c6ab1b26bede1cfcafd5da91a0d7bfabaaba4ac6
Eugene <vip.tonuvmartini@mail.ru>
2024-01-22 18:56:19 +0300

    Added crontab task to update users trophies

--- src/entyPoints/cron.ts
+++ src/entyPoints/cron.ts
@@ -1,11 +1,52 @@
+import { UserDao } from "@orm/dao/UserDao";
 import { battleLogService } from "@services/battle-logs";
+import { brawlStarsService } from "@services/brawl-stars/api";
 import cron from "node-cron";
 
 export async function initCron() {
-  // execute when server starts;
   await battleLogService.updateUsersStat();
   await battleLogService.removeOld(30);
   //
   cron.schedule("*/20 * * * *", battleLogService.updateUsersStat);
   cron.schedule("0 1 * * *", () => battleLogService.removeOld(30));
+  cron.schedule("0 1 * * *", async () => {
+    const dao = new UserDao();
+
+    const linkedUsers = await dao.getAllLinkedUsers();
+    linkedUsers.forEach(async (user) => {
+      const profileData = await brawlStarsService.players.getPlayerInfo(
+        user.player_tag!
+      );
+      user.trophies.day = profileData.trophies;
+
+      await user.trophies.save();
+    });
+  });
+  cron.schedule("0 0 * * MON", async () => {
+    const dao = new UserDao();
+
+    const linkedUsers = await dao.getAllLinkedUsers();
+    linkedUsers.forEach(async (user) => {
+      const profileData = await brawlStarsService.players.getPlayerInfo(
+        user.player_tag!
+      );
+      user.trophies.week = profileData.trophies;
+
+      await user.trophies.save();
+    });
+  });
+
+  cron.schedule("0 0 1 * *", async () => {
+    const dao = new UserDao();
+
+    const linkedUsers = await dao.getAllLinkedUsers();
+    linkedUsers.forEach(async (user) => {
+      const profileData = await brawlStarsService.players.getPlayerInfo(
+        user.player_tag!
+      );
+      user.trophies.month = profileData.trophies;
+
+      await user.trophies.save();
+    });
+  });
 }

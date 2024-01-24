3761f21ec29ee7686e71401a1ee05b746aa7a9d7
Eugene <vip.tonuvmartini@mail.ru>
2024-01-24 18:09:49 +0300

    Update CRON timezone

--- src/entyPoints/cron.ts
+++ src/entyPoints/cron.ts
@@ -1,22 +1,31 @@
-import { UserDao } from "@orm/dao/UserDao";
-import { User } from "@orm/models/User";
 import { battleLogService } from "@services/battle-logs";
-import { brawlStarsService } from "@services/brawl-stars/api";
 import { userService } from "@services/user";
 import cron from "node-cron";
 
 export async function initCron() {
   await battleLogService.updateUsersStat();
   await battleLogService.removeOld(30);
-  //
-  cron.schedule("*/20 * * * *", battleLogService.updateUsersStat);
-  cron.schedule("0 1 * * *", () => battleLogService.removeOld(30));
+
+  cron.schedule("*/20 * * * *", battleLogService.updateUsersStat, {
+    timezone: "Europe/Moscow"
+  });
+  cron.schedule("0 1 * * *", () => battleLogService.removeOld(30), {
+    timezone: "Europe/Moscow"
+  });
   cron.schedule("0 1 * * *", async () => {
     await userService.updateMysteryPointsToUsers();
     await userService.updateAllUsersTrophies("day");
+  }, {
+    timezone: "Europe/Moscow"
   });
   cron.schedule("0 0 * * MON", () =>
-    userService.updateAllUsersTrophies("week")
+    userService.updateAllUsersTrophies("week"),
+    {
+      timezone: "Europe/Moscow"
+    }
   );
-  cron.schedule("0 0 1 * *", () => userService.updateAllUsersTrophies("month"));
+  cron.schedule("0 0 1 * *", () => userService.updateAllUsersTrophies("month"),
+  {
+    timezone: "Europe/Moscow"
+  });
 }

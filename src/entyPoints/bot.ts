1ca3d11c00491f2032e1af277ea9646f409857a0
Eugene <vip.tonuvmartini@mail.ru>
2024-01-28 16:22:31 +0300

    Add catching errors on bot level

--- src/entyPoints/bot.ts
+++ src/entyPoints/bot.ts
@@ -1,7 +1,11 @@
+import { logger } from "@helpers/logs";
 import { bot } from "@services/bot";
 import { brawlStarsComposer } from "@services/bot/commands";
 
 export async function initBot() {
   bot.launch();
   bot.use(brawlStarsComposer);
+  bot.catch((error) => {
+    logger.error(error);
+  });
 }
